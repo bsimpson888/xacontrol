72cd6b9864cb5d04e492cbb9829e6f5bf0443962
Marco <bartel@electronic-shop.lu>
2015-12-08 10:20:49 +0100

    xacontrol-1 Ersten Skeleton erstellen mit Basis Funktion

--- src/xacontrol.py
+++ src/xacontrol.py
@@ -2,3 +2,12 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Marco Bartel'
+
+
+class xacontrol(object):
+    def __init__(self):
+        print "starting xacontrol..."
+
+
+if __name__ == '__main__':
+    xac = xacontrol()
